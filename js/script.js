79a7e35a5e2e8d237e4cc498ab197991716c832e
provash10 <provash20cb@gmail.com>
2024-11-26 02:06:56 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// function grade(){
+//     let marks = document.getElementById('marks' ).value;
+
+//     if(parseFloat(marks) >= 80){
+//         document.getElementById("result").innerHTML = "The Grade is = A+";
+//     }
+//     else if(parseFloat(marks) >= 70){
+//         document.getElementById('result').innerHTML = "The Grade is = A";
+//     }
+//     else if(parseFloat(marks) >= 60){
+//         document.getElementById('result').innerHTML = "The Grade is = A-";
+//     }
+//     else if(parseFloat(marks) >= 50){
+//         document.getElementById('result').innerHTML = "The Grade is = B";
+//     }
+//     else if(parseFloat(marks) >= 40){
+//         document.getElementById('result').innerHTML = "The Grade is = C";
+//     }
+//     else if(parseFloat(marks) >= 33){
+//         document.getElementById('result').innerHTML = "The Grade is = D";
+//     }
+
+//     else {
+//         document.getElementById('result').innerHTML = "Failed";
+//     }
+// }
